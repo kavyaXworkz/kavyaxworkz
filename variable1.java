455cf1d9a7cfc491951e6946ef5a9e07343da07a
kavyaXworkz <uttangikavya.xworkz@gmail.com>
2021-01-02 20:06:04 +0530

    adding the programs of variable

---
+++ variable1.java
@@ -0,0 +1,13 @@
+class Variable1
+{
+public static void main(String args[])
+{
+int k1=20;
+char k2='a';
+String k3="Kavya";
+
+System.out.println(k1);
+System.out.println(k2);
+System.out.println(k3);
+}
+}
---
+++ variable2.java
@@ -0,0 +1,17 @@
+ class Variable2
+{
+public static void main(String args[])
+{
+	String name="iskon";
+    String adress="rajajinagar";
+    String time="12";
+templeVisit(name,adress,time);
+}
+static void templeVisit(String name,String adress,String time)
+{
+System.out.println("to get a peace");
+System.out.println(name);
+System.out.println(adress);
+System.out.println(time);
+}
+}
